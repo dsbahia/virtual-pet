53fddd4dbf5532c12a530f508e8b02ebbe252ec7
dsbahia <129111782+dsbahia@users.noreply.github.com>
2023-05-10 21:12:12 +0100

    Refactored pet.js to use prototype

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -15,6 +15,17 @@ describe('constructor', () => {
       pet.growUp();
       expect(pet.age).toEqual(1);
     });
+
+    it('has an initial hunger of 0', () => {
+      const pet = new Pet('Fido');
+      expect(pet.hunger).toEqual(0);
+    });
+
+    it('has an initial fitness of 10', () => {
+      const pet = new Pet('Fido');
+      expect(pet.fitness).toEqual(10);
+    });
+
 });
 
 describe('growUp', () => {
@@ -23,4 +34,17 @@ describe('growUp', () => {
     pet.growUp();
     expect(pet.age).toEqual(1);
   });
+
+  it('this method should increase the hunger by 5', () => {
+    const pet = new Pet('Fido');
+    pet.growUp();
+    expect(pet.hunger).toEqual(5);
+  });
+
+  it('this method should decrease the fitness by 3', () => {
+    const pet = new Pet('Fido');
+    pet.growUp();
+    expect(pet.fitness).toEqual(7);
+  });
+
 });
