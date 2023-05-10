215bebd518b9e5f05a707b0f53ec0169b48d52e5
dsbahia <129111782+dsbahia@users.noreply.github.com>
2023-05-10 20:34:26 +0100

    Wrote growUp method in Pet function and associated tests

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -7,7 +7,20 @@ describe('constructor', () => {
 
     it('sets the name property', () => {
       const pet = new Pet('Fido');
-  
       expect(pet.name).toEqual('Fido');
     });
+
+    it('has an initial age of 0', () => {
+      const pet = new Pet('Fido');
+      pet.growUp();
+      expect(pet.age).toEqual(1);
+    });
+});
+
+describe('growUp', () => {
+  it('increments the age by 1', () => {
+    const pet = new Pet('Fido'); 
+    pet.growUp();
+    expect(pet.age).toEqual(1);
   });
+});
