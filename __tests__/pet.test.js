b6d1c0655918d26a77f9b944762f24108850b86c
dsbahia <129111782+dsbahia@users.noreply.github.com>
2023-05-15 20:35:54 +0100

    Introduced walking function with maximumFitness variable and associated test

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -47,4 +47,22 @@ describe('growUp', () => {
     expect(pet.fitness).toEqual(7);
   });
 
+  describe('Walk', () => {
+    it('increases fitness by 4', () => {
+      const pet = new Pet('fido');
+
+      pet.fitness = 4
+      pet.walk();
+
+      expect(pet.fitness).toEqual(8);
+    });
+
+    it('increases fitness to a maximum of 10', () => {
+      const pet = new Pet('fido');
+      pet.fitness = 8;
+      pet.walk();
+      expect(pet.fitness).toEqual(10);
+    });
+  });
+
 });
--- src/pet.js
+++ src/pet.js
@@ -1,3 +1,5 @@
+const maximumFitness = 10;
+
 function Pet(name) {
     this.name = name;
     this.age = 0;
@@ -10,5 +12,13 @@ Pet.prototype.growUp = function() {
     this.hunger += 5;
     this.fitness -= 3;
 };
+
+Pet.prototype.walk = function() {
+    if ((this.fitness + 4) <= maximumFitness ) {
+        this.fitness += 4;
+    } else {
+        this.fitness = maximumFitness;
+    }
+}
 
 module.exports = Pet;
